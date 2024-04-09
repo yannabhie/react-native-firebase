ac1cf6961c00e020e80fe7bcf84c3c623e6e59f9
detorresvc <detorresvc@gmail.com>
2024-04-09 17:55:47 +0800

    Update config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 
 export const  firebaseConfig = {
-  apiKey: "AIzaSyAGZsmd5XaN_8G8SYIkg9-tqrwt_kJG4ag",
-  authDomain: "sugar-monitoring.firebaseapp.com",
-  projectId: "sugar-monitoring",
-  storageBucket: "sugar-monitoring.appspot.com",
-  messagingSenderId: "463783085671",
-  appId: "1:463783085671:web:8fabf3cad1790420fbf6c2",
-  measurementId: "G-61Z271N1RJ",
-  databaseURL: "https://sugar-monitoring-default-rtdb.asia-southeast1.firebasedatabase.app",
+  apiKey: "AIzaSyCIJEi_DvZa4J-pXl6YWV8nFP40ungDBEA",
+  authDomain: "techno-e444c.firebaseapp.com",
+  databaseURL: "https://techno-e444c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "techno-e444c",
+  storageBucket: "techno-e444c.appspot.com",
+  messagingSenderId: "911612811141",
+  appId: "1:911612811141:web:8633fd8055cfe2f9f016ec",
+  measurementId: "G-1GKGCXE2XF"
 };
