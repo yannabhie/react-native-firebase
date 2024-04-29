b8036e1f40e7883ea87d37cc97a061b54fc130d1
detorresvc <detorresvc@gmail.com>
2024-04-29 19:38:47 +0800

    Add chart

--- Dashboard.js
+++ Dashboard.js
@@ -1,10 +1,20 @@
 
 
-import { ActivityIndicator, FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Pressable, SafeAreaView, StyleSheet, Text, View, Dimensions } from 'react-native';
 import { getAuth } from "firebase/auth";
 import { useEffect, useState } from 'react';
-import { getDatabase, ref, onValue, set, query, limitToLast, push, onChildAdded, orderByChild, equalTo } from "firebase/database";
+import { getDatabase, ref, onValue, set, query, limitToLast, push, onChildAdded, orderByChild, equalTo, off  } from "firebase/database";
 import dayjs from 'dayjs';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+
+
 const db = getDatabase();
 
 
@@ -21,26 +31,35 @@ const Dashboard = () => {
   const [list, setList] = useState(null);
   const auth = getAuth();
   const user = auth.currentUser;
+  const [isStartReading, setIsStartReading] = useState(false)
   
   useEffect(() => {
-    try{
-      
-      const starCountRef = ref(db, 'current-device-reading');
-      onValue(starCountRef, (snapshot) => {
-        const data = snapshot?.val();
-        setSugarLevel(data)
-        setIsLoading(false)
-
-        setTimeout(() => {
-          setSugarLevel(null)
-          setIsLoading(true)
-        }, 5000)
-      });
-    }catch(e){
-      console.log(e)
-
+    const starCountRef = ref(db, 'current-device-reading');
+    if(!!isStartReading){
+      try{
+        
+        onValue(starCountRef, (snapshot) => {
+          const data = snapshot?.val();
+          if(data){
+            setSugarLevel(data)
+            setIsLoading(false)
+          }
+          
+          setTimeout(() => {
+            setSugarLevel(null)
+            setIsLoading(true)
+          }, 5000)
+        });
+      }catch(e){
+        console.log(e)
+      }
     }
-  }, [])
+
+    if(!isStartReading){
+      off(starCountRef)
+    }
+    
+  }, [isStartReading])
 
   useEffect(() => {
     if(sugarLevel){
@@ -59,33 +78,89 @@ const Dashboard = () => {
 
   useEffect(() => {
     const listRef = ref(db, 'sugar-readings');
-    const q = query(listRef, orderByChild('uid'), equalTo(user?.uid));
+    const q = query(listRef, orderByChild('uid'), equalTo(user?.uid), limitToLast(10));
     onValue(q, (snapshot) => {
       setList(snapshot?.val())
     })
       
   },[])
 
+  const onHandleSend = () => {
+    setIsStartReading(prev => {
+      
+      if(!!prev){
+        const currentDeviceReading =  ref(db, `current-device-reading`)
+        set(currentDeviceReading, null);
+        setIsLoading(true)
+      }
+
+      return !prev
+    })
+  }
+
   
   return (
     <View style={styles.container}>
-      {isLoading ?
+      {isStartReading && isLoading ?
       <ActivityIndicator size="large" /> : null}
-      {isLoading ? 
+      {isStartReading &&  isLoading ? 
         <Text>Waiting for device result...</Text> : null}
-      {sugarLevel ?
+      {isStartReading && sugarLevel ?
       <Text style={styles.textResult}>{sugarLevel}</Text> : null}
-      {sugarLevel ?
+      {isStartReading && sugarLevel ?
       <Text>Current Sugar Reading</Text> : null}
-      <Text>{user?.providerData?.[0]?.email}</Text>
-      
-      <SafeAreaView style={styles.list}>
+      <View style={{ margin: 10 }}>
+        <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'blue' }}>{user?.providerData?.[0]?.email}</Text>
+      </View>
+      <Pressable style={!isStartReading ? styles.startButton : styles.stopButton} onPress={onHandleSend}>
+        <Text style={{ color: 'white'}}>{!isStartReading ? 'START' : 'STOP'}</Text>
+      </Pressable>
+      {Object.keys(list || [])?.length > 0 ?
+      <View>
+        <LineChart
+            data={{
+              labels: Object.keys(list || []).map((x) => dayjs(x).format('HH:mm')),
+              datasets: [
+                {
+                  data: Object.values(list || []).map(x => parseFloat(x?.value))
+                }
+              ]
+            }}
+            width={Dimensions.get("window").width} // from react-native
+            height={220}
+            yAxisLabel=""
+            yAxisSuffix=""
+            yAxisInterval={1} // optional, defaults to 1
+            chartConfig={{
+              backgroundColor: "#e26a00",
+              backgroundGradientFrom: "#fb8c00",
+              backgroundGradientTo: "#ffa726",
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16
+              },
+              propsForDots: {
+                r: "6",
+                strokeWidth: "2",
+                stroke: "#ffa726"
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
+      </View> : null}
+      {/* <SafeAreaView style={styles.list}>
         <FlatList
           data={Object.values(list || {})}
           renderItem={({item}) => <Item date={item?.timestamp} value={item.value} />}
           keyExtractor={item => item.id}
         />
-      </SafeAreaView>
+      </SafeAreaView> */}
     </View>
   )
 }
@@ -123,6 +198,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     
   },
+  startButton: {
+    backgroundColor: 'blue',
+    color: 'white',
+    padding: 10,
+    borderRadius: 5,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  stopButton: {
+    backgroundColor: 'red',
+    color: 'white',
+    padding: 10,
+    borderRadius: 5,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 20,
+    marginBottom: 20
+  }
   
 });
 
--- config.js
+++ config.js
@@ -8,4 +8,13 @@ export const  firebaseConfig = {
   messagingSenderId: "911612811141",
   appId: "1:911612811141:web:8633fd8055cfe2f9f016ec",
   measurementId: "G-1GKGCXE2XF"
+
+  // apiKey: "AIzaSyAGZsmd5XaN_8G8SYIkg9-tqrwt_kJG4ag",
+  // authDomain: "sugar-monitoring.firebaseapp.com",
+  // databaseURL: "https://sugar-monitoring-default-rtdb.asia-southeast1.firebasedatabase.app",
+  // projectId: "sugar-monitoring",
+  // storageBucket: "sugar-monitoring.appspot.com",
+  // messagingSenderId: "463783085671",
+  // appId: "1:463783085671:web:8fabf3cad1790420fbf6c2",
+  // measurementId: "G-61Z271N1RJ"
 };
